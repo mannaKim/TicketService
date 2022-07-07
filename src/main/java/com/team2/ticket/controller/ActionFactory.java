27582cfee283237ea255fddfb2eb0c6ce686eb2a
mannaKim <gkssk2286@naver.com>
2022-07-07 09:42:20 +0900

    Ticket first commit

---
+++ src/main/java/com/team2/ticket/controller/ActionFactory.java
@@ -0,0 +1,20 @@
+package com.team2.ticket.controller;
+
+import com.team2.ticket.controller.action.Action;
+
+public class ActionFactory {
+	
+	//Singleton Pattern
+	private ActionFactory() {}
+	private static ActionFactory itc = new ActionFactory();
+	public static ActionFactory getInstance() {return itc;}
+	
+	//command 값에 따라 Action 객체 변화
+	public Action getAction(String command) {
+		Action ac = null;
+		
+		
+		
+		return ac;
+	}
+}
