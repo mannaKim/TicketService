0a15ef3d54505fea46932c49a89d5c170a254071
mannaKim <gkssk2286@naver.com>
2022-07-07 15:06:21 +0900

    Login

---
+++ src/main/java/com/team2/ticket/dao/MemberDao.java
@@ -0,0 +1,44 @@
+package com.team2.ticket.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.team2.ticket.dto.MemberVO;
+import com.team2.ticket.util.Dbman;
+
+public class MemberDao {
+	private MemberDao() {}
+	private static MemberDao itc = new MemberDao();
+	public static MemberDao getInstance() {return itc;}
+	
+	Connection con = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
+	
+	public MemberVO getMember(String id) {
+		MemberVO mvo = null;
+		String sql = "select * from member where id=?";
+		con = Dbman.getConnection();
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				mvo = new MemberVO();
+				mvo.setId(rs.getString("id"));
+				mvo.setPwd(rs.getString("pwd"));
+				mvo.setName(rs.getString("name"));
+				mvo.setEmail(rs.getString("email"));
+				mvo.setZip_code(rs.getString("zip_code"));
+				mvo.setAddress1(rs.getString("address1"));
+				mvo.setAddress2(rs.getString("address2"));
+				mvo.setPhone(rs.getString("phone"));
+				mvo.setIndate(rs.getTimestamp("indate"));
+			}
+		} catch (SQLException e) { e.printStackTrace();
+		} finally { Dbman.close(con, pstmt, rs); }
+		return mvo;
+	}
+}
---
+++ src/main/java/com/team2/ticket/dto/MemberVO.java
@@ -0,0 +1,70 @@
+package com.team2.ticket.dto;
+
+import java.sql.Timestamp;
+
+public class MemberVO {
+	private String id;
+	private String pwd;
+	private String name;
+	private String email;
+	private String zip_code;
+	private String address1;
+	private String address2;
+	private String phone;
+	private Timestamp indate;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getZip_code() {
+		return zip_code;
+	}
+	public void setZip_code(String zip_code) {
+		this.zip_code = zip_code;
+	}
+	public String getAddress1() {
+		return address1;
+	}
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+	public String getAddress2() {
+		return address2;
+	}
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public Timestamp getIndate() {
+		return indate;
+	}
+	public void setIndate(Timestamp indate) {
+		this.indate = indate;
+	}
+}
